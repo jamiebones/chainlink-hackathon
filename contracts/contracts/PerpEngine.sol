72c6b3aaa2917a52d882911def99e46bb187189b
naraharirohith <rohit.narahari@gmail.com>
2025-06-12 00:31:25 +0530

    removing Perps folder

--- contracts/contracts/PerpEngine.sol
+++ contracts/contracts/PerpEngine.sol
@@ -5,7 +5,7 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { FixedPointMathLib } from "solmate/src/utils/FixedPointMathLib.sol";
-import { Utils } from "../../lib/Utils.sol";
+import { Utils } from "../lib/Utils.sol";
 import "hardhat/console.sol";
 
 /// @notice Minimal liquidity pool interface
