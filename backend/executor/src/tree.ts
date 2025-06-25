05606bcea7bee5a6ef5af798d08e00d4aff5fcb9
anushkasomani <somanianu956@gmail.com>
2025-06-26 00:49:47 +0530

    tree.ts and proof.ts final fix

--- backend/executor/src/tree.ts
+++ backend/executor/src/tree.ts
@@ -6,36 +6,51 @@ export interface Leaf {
   margin: bigint;
   entryFunding: bigint;
 }
-
 // Use poseidon3 for leaf hash (3 fields)
 export function hashLeaf(l: Leaf): bigint {
   return poseidon3([l.size, l.margin, l.entryFunding]);
 }
-
-
 const leafLeft: Leaf = { size: BigInt(3), margin: BigInt(-100), entryFunding: BigInt(5) };
-const leafRight: Leaf = { size: BigInt(4), margin: BigInt(120), entryFunding: BigInt(10) };
+const leafMid: Leaf = { size: BigInt(4), margin: BigInt(120), entryFunding: BigInt(10) };
+const leafRight: Leaf = { size: BigInt(4), margin: BigInt(-120), entryFunding: BigInt(10) };
+
 const zeroValue = BigInt(0);
 
 const leftHash = hashLeaf(leafLeft);
+const midHash = hashLeaf(leafMid);
 const rightHash = hashLeaf(leafRight);
 
-const depth = 1;
+const depth = 10;
 const arity = 2;
-const leaves = [leftHash, rightHash];
+const leaves = [leftHash, midHash, rightHash];
 
 // Use poseidon2 for the IMT hash function (arity=2)
 const tree = new IMT(poseidon2, depth, zeroValue, arity, leaves);
 
-console.log("Leaf hashes:", leaves);
+console.log("Tree is", tree);
 console.log("Merkle root:", tree.root);
 
-const proof = tree.createProof(0);
-console.log("Proof valid:", tree.verifyProof(proof));
-// const idx = tree.indexOf(BigInt(1707644369138053022538835966562073485769438133463321029850572729311537010102));
-// console.log(idx)
-console.log(tree.leaves)
+// const proof = tree.createProof(0);
+// console.log("Proof valid:", tree.verifyProof(proof));
+// console.log(tree.leaves)
+// tree.insert(hashLeaf({size: BigInt(5), margin: BigInt(200), entryFunding: BigInt(15)}));
+// console.log("New Merkle root after insertion:", tree.root);
+// console.log("New Merkle leaves after insertion:", tree);
+// tree.delete(2)
+// console.log("New Merkle tree after deletion:", tree);
 
 export function currentRoot(){
   return tree.root;
 }
+
+export function insertPosition(leaf: Leaf): number{
+  tree.insert(hashLeaf(leaf));
+  return tree.indexOf(hashLeaf(leaf));
+}
+
+export function deletePosition(hash: bigint){
+  const index= tree.indexOf(hash)
+  tree.delete(index)
+}
+// deletePosition(15215956860192754867003942406872706015577979927073229954434143459039467021244n)
+// console.log(tree)
